0eb2bca2be994d65ce5514e858ab2c15288bcf2c
azeeshanssi <azeeshan@ssidecisions.com>
2023-08-11 16:41:55 +0500

    Arrow Animation Added for Table

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,83 +1,101 @@
 <template>
-       
-      <table class="w-5/6">
-        <thead>
-          <tr>
-            <th
-              class="bg-blue-600 hover:bg-blue-300 capitalize hover:cursor-pointer text-white border px-4 py-2"
-              v-for="header in selectedHeaders"
-              :key="header"
-              @click="sortData(header)"
-            >
-              <!-- {{ formatHeader(header) }} -->
-              {{  header.replaceAll('_',' ') }}
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr
-            v-for="(coin, coinIndex) in displayData"
-            :key="coinIndex"
-            :class="[{ 'bg-blue-100': coinIndex % 2 === 0 }, 'hover:bg-blue-300']"
+    <table class="w-5/6">
+      <thead>
+        <tr>
+          <th
+            class="bg-blue-600 hover:bg-blue-300 capitalize hover:cursor-pointer text-white border px-4 py-2"
+            v-for="header in selectedHeaders"
+            :key="header"
+            @click="sortData(header)"
           >
-            <td v-for="header in selectedHeaders" :key="header" class="border px-4 py-2">
-              <template v-if="coin[header] != null">
-                <template v-if="header === 'image'">
-                  <img :src="coin[header]" alt="Coin Image" class="w-10 h-10" />
-                </template>
-                <template v-else>
-                  {{ coin[header] }}
-                </template>
+            {{ formatHeader(header) }}
+            <svg
+              :class="{ 'transform rotate-180': sortBy === header && sortOrder[header] === 'desc' }"
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5 text-gray-600 ml-2"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M19 9l-7 7-7-7"
+              />
+            </svg>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="(coin, coinIndex) in displayData"
+          :key="coinIndex"
+          :class="[{ 'bg-blue-100': coinIndex % 2 === 0 }, 'hover:bg-blue-300']"
+        >
+          <td
+            v-for="header in selectedHeaders"
+            :key="header"
+            class="border px-4 py-2 text-center"
+          >
+            <template v-if="coin[header] != null">
+              <template v-if="header === 'image'">
+                <img :src="coin[header]" alt="Coin Image" class="w-10 h-10" />
               </template>
-              <template v-else> <div class="text-center">-</div> </template>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-</template>
-
-<script setup>
-import { ref } from 'vue';
-const props = defineProps(['selectedHeader','printData']);
-const selectedHeaders=ref(props.selectedHeader);
-const displayData=ref(props.printData);;
-const coins = ref([]);
-coins.value=displayData.value;
-//Formatting logical concern
-const formatHeader = (header) => {
-  const words = header.split('_').map((word) => {
-    return word.charAt(0).toUpperCase() + word.slice(1)
-  })
-  return words.join(' ')
-}
-//Sorting logical concern
-const sortBy = ref(null)
-const sortAsc = ref(true)
-
-const sortData = (header) => {
-  if (sortBy.value === header) {
-    sortAsc.value = !sortAsc.value
-  } else {
-    sortAsc.value = true
-    sortBy.value = header
-  }
-
-  if (!sortBy.value) {
-    sortedData.value = coins.value
-    return
-  }
-
-  displayData.value = [...coins.value].sort((a, b) => {
-    const valueA = a[header]
-    const valueB = b[header]
-
-    if (typeof valueA === 'string' && typeof valueB === 'string') {
-      return sortAsc.value ? valueA.localeCompare(valueB) : valueB.localeCompare(valueA)
+              <template v-else>
+                {{ coin[header] }}
+              </template>
+            </template>
+            <template v-else>
+              <div class="text-center">-</div>
+            </template>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  
+  const props = defineProps(['selectedHeader', 'printData']);
+  const selectedHeaders = ref(props.selectedHeader);
+  const displayData = ref(props.printData);
+  const coins = ref([]);
+  coins.value = displayData.value;
+  
+  // Formatting logical concern
+  const formatHeader = (header) => {
+    const words = header.split('_').map((word) => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+    return words.join(' ');
+  };
+  
+  // Sorting logical concern
+  const sortBy = ref(null);
+  const sortOrder = ref({}); // Keep track of the sorting order for each header
+  
+  const sortData = (header) => {
+    if (sortBy.value === header) {
+      sortOrder.value[header] = sortOrder.value[header] === 'asc' ? 'desc' : 'asc';
     } else {
-      return sortAsc.value ? valueA - valueB : valueB - valueA
+      sortOrder.value[header] = 'asc';
+      sortBy.value = header;
     }
-  })
-}
-
-
-</script>
+  
+    displayData.value = [...coins.value].sort((a, b) => {
+      const valueA = a[header];
+      const valueB = b[header];
+  
+      if (typeof valueA === 'string' && typeof valueB === 'string') {
+        return sortOrder.value[header] === 'asc'
+          ? valueA.localeCompare(valueB)
+          : valueB.localeCompare(valueA);
+      } else {
+        return sortOrder.value[header] === 'asc' ? valueA - valueB : valueB - valueA;
+      }
+    });
+  };
+  </script>
+  
