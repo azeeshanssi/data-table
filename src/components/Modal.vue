fb2b7b4c418af0f6ae63bc23726c1c1b0befabfd
azeeshanssi <azeeshan@ssidecisions.com>
2023-08-15 10:27:42 +0500

    Changed to checkbox for multiple select,deselect

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -6,16 +6,17 @@
     <div id="card" class="bg-white p-12 rounded-lg shadow-lg h-90 flex flex-col">
       <h1 class="text-center text-xl">Select Columns</h1>
       <div class="mt-2 w-full">
-        <label v-for="(header, index) in dropdownOptions" :key="index" class="flex items-center capitalize">
+         <label v-for="(header, index) in dropdownOptions" :key="index" class="flex items-center capitalize">
           <input
-            type="radio"
+            type="checkbox"
             :value="header"
             :checked="selected.includes(header)"
-            class="form-radio  text-slate-600 mr-1 "
+            class="  text-slate-600 mr-1 "
             @change="handleRadioChange(header)"
           />
           {{ header.replaceAll('_',' ') }}
-        </label>
+        </label> 
+      
       </div>
 
       <button
